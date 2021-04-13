e41fd2d9ecc580f5827a9086bc4bedfa0b395dcc
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-04-13 22:06:54 +0100

    cutting out the rubbish, setting up component directories

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <NavBar />
 </template>
+
+<script>
+import NavBar from './components/NavBar/NavBar.vue';
+
+export default {
+  name: 'Skeleton',
+  components: {
+    NavBar,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
