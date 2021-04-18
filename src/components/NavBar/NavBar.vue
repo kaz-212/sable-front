2463611ef1eea0a54577b92ed054b29c4fd310ae
Karan Basu <karan.basu16@gmail.com>
2021-04-18 19:02:52 +0100

    basic nav stylings

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,9 +1,13 @@
 <template>
   <nav id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link :to="{ name: 'Shows' }">Shows</router-link>
-    <router-link :to="{ name: 'About' }">About</router-link>
-    <router-link :to="{ name: 'Schedule' }">Schedule</router-link>
+    <div class="logo">
+      <router-link class="nav-link" :to="{ name: 'Home' }">sable radio</router-link>
+    </div>
+    <div class="links">
+      <router-link class="nav-link" :to="{ name: 'Shows' }">shows</router-link>
+      <router-link class="nav-link" :to="{ name: 'About' }">about</router-link>
+      <router-link class="nav-link" :to="{ name: 'Schedule' }">schedule</router-link>
+    </div>
     <!-- <h2>{{ currentShow }}</h2> -->
     <SecondaryNav />
   </nav>
@@ -19,3 +23,39 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+#nav {
+  display: block;
+  width: 100vw;
+  height: $navHeight;
+  border-bottom: 1px solid black;
+  display: flex;
+
+  .nav-link {
+    text-decoration: none;
+    color: $primaryTextColour;
+    margin: 0 40px;
+    padding: 0 5px;
+    font-size: $navLinkSize;
+    &:hover {
+      background-color: $hoverColour;
+    }
+  }
+
+  .logo {
+    margin-left: 100px;
+    padding-right: 100px;
+    border-right: 1px solid black;
+    display: flex;
+    align-items: center;
+  }
+
+  .links {
+    margin-left: auto;
+    margin-right: 100px;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
