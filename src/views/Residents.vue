19895278a017cbbf7cb8ed61c30febe2fabf1769
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-04-27 21:34:18 +0100

    residents page linked to api

--- src/views/Residents.vue
+++ src/views/Residents.vue
@@ -3,7 +3,7 @@
     <div class="resident" v-bind:key="resident.id" v-for="resident in residents">
       <h1>{{ resident.name }}</h1>
       <p>{{ resident }}</p>
-      <img :src="resident.image" alt="">
+      <img :src="resident.image_url" alt="" width=500>
     </div>
   </div>
 </template>
