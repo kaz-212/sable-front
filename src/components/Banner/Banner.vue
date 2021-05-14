449a328d1108b889428d7deec56326eea41c3f70
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-05-15 00:51:16 +0100

    working on more info

--- src/components/Banner/Banner.vue
+++ src/components/Banner/Banner.vue
@@ -11,17 +11,25 @@
     </div>
     <div class="img-wrapper" v-if="slides[selectedImg].image_url">
       <img :src="slides[selectedImg].image_url" alt="banner-image" />
+      <div v-if="slides[selectedImg].link_url" class="more-info">
+        <MoreInfo :link_url="slides[selectedImg].link_url" />
+      </div>
     </div>
+    <button>Hello</button>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import MoreInfo from './MoreInfo.vue';
 
 const bannerImgURL = 'https://sable-radio.herokuapp.com/api/banner';
 
 export default {
   name: 'Banner',
+  components: {
+    MoreInfo
+  },
   data() {
     return {
       selectedImg: 0,
---
+++ src/components/Banner/MoreInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="more-info">
+    <button>Hi {{ link_url }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MoreInfo',
+  props: {
+    link_url: String
+  }
+};
+</script>
+
+<style>
+.more-info {
+  z-index: 10000;
+  background-color: red;
+}
+</style>
