600b4b555501050b269b206baf4a7aeeca91c8ed
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-05-21 17:50:15 +0100

    residents coming soon

--- src/views/Residents.vue
+++ src/views/Residents.vue
@@ -1,5 +1,8 @@
 <template>
-  <div v-if="residents">
+  <div class="blog">
+    <h1>Residents coming soon!</h1>
+  </div>
+  <!-- <div v-if="residents">
     <grid-template header="residents">
       <div class="grid">
         <ResCard
@@ -13,26 +16,35 @@
         />
       </div>
     </grid-template>
-  </div>
+  </div> -->
 </template>
 
 <script>
-import ResCard from '@/components/Cards/ResCard.vue';
-import GridTemplate from '@/components/Templates/GridTemplate.vue';
+// import ResCard from '@/components/Cards/ResCard.vue';
+// import GridTemplate from '@/components/Templates/GridTemplate.vue';
 
-export default {
-  name: 'Residents',
-  components: {
-    ResCard,
-    GridTemplate
-  },
-  computed: {
-    residents() {
-      return this.$store.getters['residents/getResidents'];
-    }
-  },
-  async created() {
-    await this.$store.dispatch('residents/fetchResidents');
-  }
-};
+// export default {
+//   name: 'Residents',
+//   components: {
+//     ResCard,
+//     GridTemplate
+//   },
+//   computed: {
+//     residents() {
+//       return this.$store.getters['residents/getResidents'];
+//     }
+//   },
+//   async created() {
+//     await this.$store.dispatch('residents/fetchResidents');
+//   }
+// };
 </script>
+
+<style lang="scss" scoped>
+.blog {
+  height: calc(100vh - (#{$navHeight} + #{$footerHeight * 2} + 3px));
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
