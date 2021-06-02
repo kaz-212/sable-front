035e3cc3f8028873b12f7b2a5233eea5323f32e6
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-06-03 00:41:21 +0100

    workable

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
-if (window.location.protocol !== 'https:') window.location.href = 'https://www.sableradio.live';
+// uncomment below for every redeploy
+// if (window.location.protocol !== 'https:') window.location.href = 'https://www.sableradio.live';
 
 new Vue({
   router,
