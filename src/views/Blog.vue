ca9c19fd6ccfc96a2583bfcc8b3604d7fa739566
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-06-06 17:29:27 +0100

    search working too

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="blog">
+    <h1>
+      Woah there cowboy!
+      <br />
+      This bit of the site isn’t quite ready...
+      <br />
+      come back in a month and there will be more fun stuff here.
+    </h1>
+  </div>
+  <!-- <div class="blog">
     <grid-template header="blogs">
       <div class="grid">
         <BlogCard
@@ -12,30 +21,36 @@
           class="grid-item"
         />
       </div>
-    </grid-template>
-  </div>
+    </grid-template> -->
+  <!-- </div> -->
 </template>
 
 <script>
-import BlogCard from '@/components/Cards/BlogCard.vue';
-import GridTemplate from '@/components/Templates/GridTemplate.vue';
+// import BlogCard from '@/components/Cards/BlogCard.vue';
+// import GridTemplate from '@/components/Templates/GridTemplate.vue';
 
-export default {
-  name: 'Blogs',
-  components: {
-    BlogCard,
-    GridTemplate
-  },
-  async created() {
-    await this.$store.dispatch('blogs/fetchBlogs');
-  },
-  computed: {
-    blogs() {
-      return this.$store.getters['blogs/getBlogs'];
-    }
-  }
-};
+// export default {
+//   name: 'Blogs',
+//   components: {
+//     BlogCard,
+//     GridTemplate
+//   },
+//   async created() {
+//     await this.$store.dispatch('blogs/fetchBlogs');
+//   },
+//   computed: {
+//     blogs() {
+//       return this.$store.getters['blogs/getBlogs'];
+//     }
+//   }
+// };
 </script>
 
 <style lang="scss" scoped>
+.blog {
+  height: calc(100vh - (#{$navHeight} + #{$footerHeight * 2} + 3px));
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
